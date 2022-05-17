5e4f8712a0282e3437e644e80f0450b4478488bb
NicolasArias <ariasmec06@gmail.com>
2022-05-17 18:53:52 -0300

    punto 1 Invertur numero

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Moon");
+﻿//Programa para invertir un numero
+int numero;
+int result;
+Console.WriteLine("Ingrese el numero para invertie; debe se ser mayor que 0");
+numero= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(numero);
+
+if (numero > 0)
+{
+    result= InvertirNumero(numero);
+    Console.WriteLine("El numero invertido es: " + result);
+}else{
+    Console.WriteLine("No es posible invertir el numero");
+}
+
+int InvertirNumero(int numero){
+    int resultado= 0;
+    while (numero > 0)
+    {
+        resultado= resultado * 10 + numero % 10;
+        numero /= 10;
+    }
+    return resultado;
+}
